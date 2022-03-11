70f4e04997ac9d5d605316785479866ddbec89cc
Gwanghyun Choi <66342017+helirang@users.noreply.github.com>
2022-03-11 23:53:52 +0900

    Create RectangleStar.cpp
    직사각형 별찍기

---
+++ Lv1/RectangleStar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    string temp;
+    temp = 
+    for(int i=0; i<a;i++){
+        for(int j=0;j<b;j++){
+            
+        }
+    }
+    cin >> a >> b;
+    cout << temp << endl;
+    return 0;
+}
