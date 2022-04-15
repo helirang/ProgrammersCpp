fd3811680f767dd01c978dd3ff6aae434e1c19b3
Gwanghyun Choi <66342017+helirang@users.noreply.github.com>
2022-04-16 00:45:03 +0900

    Update StrDesc.cpp

--- Lv1/StrDesc.cpp
+++ Lv1/StrDesc.cpp
@@ -5,18 +5,29 @@ using namespace std;
 
 string solution(string s) {
     string answer = "";
-    
+    string tempIdx = "";
     for(int i=0; i<s.size(); i++){
         int bigOne = 0;
-        for(char element : s){
-            if(answer.size()==0)
-            bigOne = (int)element > bigOne ? (int)element : bigOne;
-            else 
-            bigOne = (int)element > bigOne && element < answer[answer.size()-1] ?
-                (int)element : bigOne;
+        int idx = 0;
+        for(int j=0; j<s.size();j++)
+        {
+            int num = (int)s[j];
+            if(answer.size()==0 && num > bigOne){
+                bigOne = num;
+                idx = j;
+            }else if(num >= bigOne && num <= answer[answer.size()-1]){
+                bool bChk = false;
+                for(char element : tempIdx){
+                    bChk = (int)element == j|| bChk ? true : false;
+                }
+                if(!bChk){
+                    bigOne = num;
+                    idx = j;
+                }
+            }
         }
+        tempIdx += (char)idx;
         answer += (char)bigOne;
     }
-   
     return answer;
 }
