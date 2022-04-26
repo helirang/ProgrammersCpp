397a75380ceb16927f6f5517bfe23aa387df1438
Gwanghyun Choi <66342017+helirang@users.noreply.github.com>
2022-04-26 23:35:56 +0900

    Create SumBetweenTwoNum.cpp
    두 정수 사이의 합

---
+++ Lv1/SumBetweenTwoNum.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    int bigNum = a >= b ? a : b;
+    int smallNum = a < b ? a : b;
+    for(int i=smallNum; i<=bigNum;i++){
+        answer+=i;
+    }
+    return answer;
+}
