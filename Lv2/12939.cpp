34af2420ccf0936abebb73395c3b84f62c8a9ace
Gwanghyun Choi <66342017+helirang@users.noreply.github.com>
2022-09-26 10:54:44 +0900

    Create 12939.cpp
    string :: npos는 -1 값을 가지는 상수
    find() 함수에 의해서 found 되지 못하는 경우 npos값이 리턴.

---
+++ Lv2/12939.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    std::string separator = " ";
+    int cur_position = 0,position = 0;
+    int min, max;
+    while (position != std::string::npos) {
+        position = s.find(separator, cur_position);
+        int len = position - cur_position;
+        int tempNum = position != std::string::npos ? 
+            stoi(s.substr(cur_position, len)) : stoi(s.substr(cur_position));
+        if (cur_position == 0) {
+            min = tempNum;
+            max = tempNum;
+        }
+        min = min > tempNum ? tempNum : min;
+        max = max < tempNum ? tempNum : max;
+        cur_position = position + 1;
+    }
+    answer = to_string(min) + " " + to_string(max);
+    return answer;
+}
