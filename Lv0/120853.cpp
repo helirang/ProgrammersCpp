e06b1b041ae607f8531c9742a6be33b9d6d3e604
Gwanghyun Choi <66342017+helirang@users.noreply.github.com>
2022-10-17 14:10:47 +0900

    Create 120853.cpp
    컨트롤 제트

---
+++ Lv0/120853.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    vector<int> zNums;
+    string tempStr = "";
+    vector<int> idxs;
+    for (int i = 0;i < s.length();i++) {
+        char ch = s[i];
+
+        if (48 <= ch && ch <= 57||ch==45) {
+            tempStr += ch;
+        }
+        else if (ch == 90) {
+            answer -= zNums.back();
+            zNums.pop_back();
+        }
+
+        if (ch == 32 || i == s.length() - 1) {
+            if (tempStr != "") {
+                answer += stoi(tempStr);
+                zNums.push_back(stoi(tempStr));
+            }
+            tempStr = "";
+        }
+    }
+    return answer;
+}
