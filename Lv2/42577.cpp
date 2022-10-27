9df425a8e194dc47aae5662222be141ec58794e4
Gwanghyun Choi <66342017+helirang@users.noreply.github.com>
2022-10-27 17:19:32 +0900

    전화번호 목록

---
+++ Lv2/42577.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    sort(phone_book.begin(),phone_book.end());
+    for(int i=0; i<phone_book.size();i++){
+        for(int j=i+1; j<phone_book.size();j++){
+            if(phone_book[i].length()>phone_book[j].length()) continue;
+
+            bool isBreak = false;
+            for(int k =0; k<phone_book[i].size();k++){
+                if(phone_book[i][k]!=phone_book[j][k]) {
+                    isBreak = true;
+                    break;
+                }
+            }
+            if(isBreak) break;
+            else return false;
+        }
+    }
+    return answer;
+}
